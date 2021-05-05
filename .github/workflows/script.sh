0db736c4d293a7a12d0965a87b80682c000a46b0
trabab <75987879+trabab@users.noreply.github.com>
2021-05-05 16:31:15 +0530

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -8,7 +8,7 @@ def updateJsonFile():
 
     with open('opsgenie_team/overrides.json', 'r') as f:
         data = json.load(f)
-        data["CREATE"]["/OpsgenieApiKey"]= "test-api-2"
+        data["CREATE"]["/OpsgenieApiKey"]= "test-api-3"
         print(data);
         with open('opsgenie_team/overrides.json', 'w') as f1:
             json.dump(data, f1, indent=4)
